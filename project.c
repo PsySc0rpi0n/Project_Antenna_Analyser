5d82f66eb6743dc8c536a1073a6820ad54a96065
PsySc0rpi0n <totallacc@gmail.com>
2017-09-07 15:51:06 +0100

    Next attemp for the whole code

--- project.c
+++ project.c
@@ -2,12 +2,15 @@
 #include <inttypes.h>
 #include <stdlib.h>
 #include <util/delay.h>
+#include <string.h>
 
 #include "signal_gen.h"
 #include "adc.h"
 #include "lcd.h"
 
 #define BUFFER 64
+
+#define MSG_VSWR_VALUE "VSWR Value:"
 
 int main(void){
    float vswr_val = 0.0;
@@ -20,18 +23,17 @@ int main(void){
 
    freq_send(10e5);
 
-
    lcd_gotoxy(3,0);
    lcd_puts("VSWR annalyser");
    _delay_ms(1000);
-
+   lcd_gotoxy(0,1);
+   lcd_puts(MSG_VSWR_VALUE);
    for( ; ;){
-      lcd_gotoxy(0,1);
-      lcd_puts("VSWR Value:");
       adc_read(&vswr_val);
-      dtostrf(vswr_val, 5, 2, tmp);
+      dtostrf(vswr_val, 7, 4, tmp);
       lcd_puts(tmp);
       _delay_ms(1000);
+      lcd_gotoxy(strlen(MSG_VSWR_VALUE), 1);
    }
    return 0;
 }
