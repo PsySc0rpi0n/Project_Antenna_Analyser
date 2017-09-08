b4079fbf74cbf22175afa3fb7d2807e7004a4bca
PsySc0rpi0n <totallacc@gmail.com>
2017-09-08 22:07:32 +0100

    Next attemp for the whole code

--- project.c
+++ project.c
@@ -10,11 +10,15 @@
 
 #define BUFFER 64
 
-#define MSG_VSWR_VALUE "VSWR Value:"
-
 int main(void){
    float vswr_val = 0.0;
    char tmp[BUFFER];
+   char msg_vswr_value[] PROGMEM = "VSWR Value:";
+   char msg_vswr_analyser[] PROGMEM = "VSWR annalyser";
+   PGM_P messages[] PROGMEM = {
+                                 msg_vswr_value,
+                                 msg_vswr_analyser
+                              };
 
    lcd_init(LCD_DISP_ON);
    lcd_clrscr();
@@ -24,16 +28,16 @@ int main(void){
    freq_send(10e5);
 
    lcd_gotoxy(3,0);
-   lcd_puts("VSWR annalyser");
+   lcd_puts((PGM_P) pgm_read_word(&(messages[1])));
    _delay_ms(1000);
    lcd_gotoxy(0,1);
-   lcd_puts(MSG_VSWR_VALUE);
+   lcd_puts((PGM_P) pgm_read_word(&(messages[0])));
    for( ; ;){
       adc_read(&vswr_val);
       dtostrf(vswr_val, 7, 4, tmp);
       lcd_puts(tmp);
       _delay_ms(1000);
-      lcd_gotoxy(strlen(MSG_VSWR_VALUE), 1);
+      lcd_gotoxy(strlen((PGM_P) pgm_read_word(&(messages[0]))), 1);
    }
    return 0;
 }
