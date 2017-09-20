e3fe0bbb81fc36c92487c5c8dc1ff58c720cdda7
José Carlos Ferreira <totallacc@gmail.com>
2017-09-20 11:34:58 +0100

    Update freq_sweep.h

--- freq_sweep.h
+++ freq_sweep.h
@@ -1,10 +1,6 @@
 #define SWEEP_STA_OFF   0
-#define SWEEP_STA_ON    1
-#define SWEEP_STA_UNDEF 2
-
-#define SWEEP_DIR_UP    1
-#define SWEEP_DIR_DOWN  0
-#define SWEEP_DIR_UNDEF 2
+#define SWEEP_STA_UP    2
+#define SWEEP_STA_DOWN  1
 
 extern volatile uint8_t sweep_sta;
 
